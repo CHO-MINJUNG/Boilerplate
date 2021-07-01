59a496e9521f8e01f8b10d7f09c05d0191c55233
MINJUNG-CHO <alswjd420663@gmail.com>
2021-07-01 17:21:54 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -28,6 +31,31 @@ const userSchema = mongoose.Schema({
     tokenExp:{
         type:Number
     }
+})
+
+//user.save가 일어나기 전에 뒤에 적재된 function실행하고
+//마지막 next()메소드를 통하여 user.save파트가 실행이 된다.
+userSchema.pre('save', function(next){
+    //req.body를 가져온다고 보면 되지?
+    var user = this;
+
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err) return next(err)
+
+        if(user.isModified('password')){
+            //plan password = user.password
+            //hash = 암호화된 비밀번호
+            bcrypt.hash(user.password,salt, function(err, hash){
+                if(err) return next(err)
+                user.password=hash
+                next()
+            })
+        }
+        
+    })
+    
+
 })
 
 const User = mongoose.model('User',userSchema)
