351c3a63b5f96110d9217942734ddca67316af92
MINJUNG-CHO <alswjd420663@gmail.com>
2021-07-06 17:44:25 +0900

    f

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -51,10 +52,34 @@ userSchema.pre('save', function(next){
                 user.password=hash
                 next()
             })
+        }else{
+            next()
         }
         
     })
     
+    userSchema.methods.comparePassword = function(plainPassword, ch){
+        //plainPassword 1234567 일 때, db에 있는 암호화된 비밀번호 가 같은지 체크해야함
+        //복호화가 불가해서 plain을 암호화한 후 db에 있는 것과 같는지 확인하는 것
+        bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+            if(err) return cb(err);
+                cb(null, isMatch)
+        })
+    }
+
+    userSchema.methods.generateToken = function(cb){
+
+        var user = this;
+        //jsonwebtoken을 이용해서 token을 생성하기
+
+        var token = jwt.sign(user._id.toHexString(), 'secretToken')
+ 
+        user.token = token
+        user.save(function(err, user){
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    }
 
 })
 
